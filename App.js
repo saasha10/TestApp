eb62d497a8dd31dc49bbcf41fe2b6df1f844f777
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-03-21 18:26:43 +0100

    Add ThemeProvider and DataContainer in App

--- App.js
+++ App.js
@@ -8,8 +8,14 @@
 
 import React from 'react'
 import styled from 'styled-components'
+import { ThemeProvider } from 'react-native-elements'
+//import provider to connect component to redux store.
+import { Provider } from 'react-redux';
+//import your store to connect your component.
+import store from './store'
 import { UIColors } from './constants/colors'
 import ModalWithHooks from './components/utilities/ModalWithHooks'
+import DataContainer from './containers/DataContainer'
 //---------------------------------------------------------------------------------------
 // Dimensions to know exactly the width and height of devices 
 // const screenWidth = Math.round(Dimensions.get('window').width)
@@ -25,13 +31,16 @@ const StyledScrollView = styled.ScrollView`
 `
 
 const App = () => {
-  return (
-    <StyledSafeAreaView>
-      <StyledScrollView>
-        <ModalWithHooks/>
-      </StyledScrollView>
-    </StyledSafeAreaView>
-
+  return (<Provider store={store}>
+    <ThemeProvider>
+      <StyledSafeAreaView>
+        <StyledScrollView>
+          <ModalWithHooks />
+        </StyledScrollView>
+        <DataContainer />
+      </StyledSafeAreaView>
+    </ThemeProvider>
+  </Provider>
   )
 }
 
