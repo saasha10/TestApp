5374ce1dc9c4decab42e9f701de2da8032794718
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-03-21 18:28:12 +0100

    Add new properties and improve style

--- components/utilities/CardProperty.js
+++ components/utilities/CardProperty.js
@@ -1,30 +1,69 @@
 import React from 'react'
-import { Card, Button } from 'react-native-elements'
-import styled from 'styled-components'
+import { TouchableNativeFeedback } from 'react-native'
+import { Card } from 'react-native-elements'
+import styled from 'styled-components/native'
+import { UIColors } from '../../constants/colors'
 
-const StyledText = styled.Text`
-    font-size: 18px;
-    color: red;
+const StyledViewTitles = styled.View`
+    flex: 1;
+`
+
+const StyledTextTitle = styled.Text`
+    font-size: 15px;
+    font-weight: bold;
+    text-transform: capitalize;
+`
+
+const StyleViewSubtitle = styled.View`
+    flex-direction: row;
+`
+
+const StyledTextTitle2 = styled.Text`
+    font-size: 12px;
+    text-transform: capitalize;
 `
 
 const StyledImage = styled.Image`
-    height: 200px;
+    height: 180px;
     width: 100%;
 `
 
 const StyledButtonContainer = styled.View`
-    flex-direction: row;
+    flex-direction: row-reverse;
     justify-content: space-between;
+    align-items: flex-start;
     margin: 10px 0;
+    margin-bottom: 20px;
 `
 
+const StyledButtonShow = styled.Button`
+    margin: 5px;
+    background: ${UIColors.green};
+    border-radius: 15px;
+    font-weight: bold !important;
+    font-size: 24px;  
+    box-shadow: 5px 10px ${UIColors.grayAlmostWhite};
+`
+
+// const priceProperty = new Intl.NumberFormat("de-DE").format(parseInt(properties.propertyOperation.operationPrice)) 
+// console.log(priceProperty)
 export default ({ properties }) => (
     <Card>
         <StyledImage source={{ uri: properties.propertyImages[0].imageUrl }} />
-        <StyledButtonContainer>
-            <Button title="Contact" />
-            <Button title="Call" />
-        </StyledButtonContainer>
-        <StyledText>{properties.propertyOperation.operationType}</StyledText>
+        <TouchableNativeFeedback
+            onPress={() => console.log("helloooooooooooooo SASHA WAKE UP!!!! ")}
+            background={TouchableNativeFeedback.SelectableBackground()}>
+            <StyledButtonContainer>
+                <StyledButtonShow title="View"/>
+                <StyledViewTitles>
+                    <StyledTextTitle>{properties.propertyOperation.operationType}</StyledTextTitle>
+                    <StyledTextTitle>{properties.propertyFeatures.featuresType}</StyledTextTitle>
+                    <StyleViewSubtitle>
+                        <StyledTextTitle2>{new Intl.NumberFormat("de-DE").format(parseInt(properties.propertyOperation.operationPrice))} €</StyledTextTitle2>
+                        <StyledTextTitle2> | {properties.propertyAddress.addressTown}</StyledTextTitle2>
+                    </StyleViewSubtitle>
+                </StyledViewTitles>
+            </StyledButtonContainer>
+        </TouchableNativeFeedback>
     </Card>
 )
