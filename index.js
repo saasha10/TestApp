103c80c48553cba01ba1b599a7f268478c5558f8
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-03-21 20:01:53 +0100

    Clean code in index

--- index.js
+++ index.js
@@ -3,30 +3,7 @@
  */
 
 import { AppRegistry } from 'react-native';
-// import React from 'react'
 import App from './App';
-// import SumExampleContainer  from './containers/SumExampleContainer'
-//-----------> *** Try with AppExample *** <--------------
-// import AppExample from './AppExample'
-// import modalWithHooks from './components/utilities/ModalWithHooks'
-//-----------> *** Try with AppExample *** <--------------
-// import RootModalContainer from './containers/RootModalContainer'
 import { name as appName } from './app.json';
-//-----------------------------------------------------------------------------------------
-//-----------> *** Try with <SumExampleContainer *** <--------------
-// import { Provider } from 'react-redux' 
-// import { createStore } from 'redux'
-// import countReducer from './reducers/countReducer'
-
-// const store = createStore(countReducer)
-
-// const RNRedux = () => (
-//     <Provider store={store}>
-//         <SumExampleContainer/>
-//     </Provider>
-// )
-//-----------> *** Try with <SumExampleContainer *** <--------------
-//-----------------------------------------------------------------------------------------
-
 
 AppRegistry.registerComponent(appName, () => App);
