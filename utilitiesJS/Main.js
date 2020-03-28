afde3633a02b9f5dd95a432478b59b67b5474d54
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-03-28 18:58:29 +0100

    Add new utility to create array with differents and unique values

---
+++ utilitiesJS/Main.js
@@ -0,0 +1,22 @@
+export default class Main {
+    constructor(){
+    }
+    //--------------------------------------------------------------------------
+    /**
+     * @param arrayList -> to iterate
+     * @param wordToSearch1 -> parameter to find something
+     */
+    newArrayDifferentValues = (arrayList, wordToSearch1, wordToSearch2) => {
+        // console.log(arrayList)
+        // console.log(wordToSearch1)
+        // console.log(wordToSearch2)
+        let result = []
+
+        arrayList.forEach(element => {            
+            if (result.indexOf(element[wordToSearch1][wordToSearch2]) == -1)
+                result.push(element[wordToSearch1][wordToSearch2])
+        });
+        //console.log(result)
+        return result
+    }
+}
