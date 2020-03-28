8c718ed92578c88ceb3de646057a074b8239079d
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-03-28 18:31:14 +0100

    Add logger to see state and actions, and getFilters

--- store/index.js
+++ store/index.js
@@ -1,11 +1,16 @@
+import Main from '../utilitiesJS/Main'
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+// Logger with default options
+import logger from 'redux-logger'
 import axios from 'axios'
 import superagent from 'superagent'
-//import XMLParser from 'react-xml-parser'
-import dataReducer from '../reducers/dataReducer'
+import rootReducers from '../reducers/index'
 import { fetchData, fetchDataFulfilled, fetchDataRejected } from '../actions/data'
+import { getFilters } from '../actions/filters'
 import { URL_HOME_BUSSINES, URL_STAR_WARS, URL_HOME_BUSSINES_JSON } from '../constants'
+
+
 
 //Define your action creators that will be responsible for asynchronouse operatiosn 
 export const getPeopleAwait = () => {
@@ -21,10 +26,10 @@ export const getPeopleAwait = () => {
             console.log('people-----------', people);
             //Now when the data is retrieved dispatch an action altering redux state.
             dispatch(fetchDataFulfilled(people.results))
-          } catch(error) {
+        } catch (error) {
             console.log('Getting People Error---------', error);
             dispatch(fetchDataRejected(error))
-          }
+        }
     }
 }
 
@@ -37,12 +42,13 @@ export const getPeopleAxios = () => {
         dispatch(fetchData(true));
         //Then get the data.
         axios.get(URL_HOME_BUSSINES_JSON).then(res => {
-            console.log('data----------->', res)
-            //Set the results to the people array.
-            // let dataParsed = new XMLParser().parseFromString(res.data)
-            // console.log("DATA -----> ", dataParsed)
-            // const properties = dataParsed.getElementsByTagName('listaPropiedades')
-            // console.log("properties", properties)
+            //console.log('data----------->', res)
+            //res.data.customerProperties.propertyFeatures.featuresType
+            let main = new Main()
+            const typeHome = main.newArrayDifferentValues(res.data.customerProperties, 'propertyFeatures', 'featuresType')
+
+            // console.log("typeHome", typeHome)
+            dispatch(getFilters({typeHome}))
             dispatch(fetchDataFulfilled(res.data))
             //Error handle the promise and set your errorMessage
         }).catch(err => dispatch(fetchDataRejected(err)))
@@ -55,14 +61,14 @@ export const getPeopleSuperAgent = () => {
         dispatch(fetchData(true));
         //Then do a get request the get the err, and response callback, if there's an error dispatch the fetchDataRejected.
         superagent.get(URL_HOME_BUSSINES)
-        //When the data is retrieved we will invoke the end method.
-        .end((err, res) => {
-            //if there is an error use our fetchDataReject
-            if(err) dispatch(fetchDataRejected(err));
-            //We will set our loading state when fetching data is successful.
-            dispatch(fetchDataFulfilled(res.body.results));
-        })
+            //When the data is retrieved we will invoke the end method.
+            .end((err, res) => {
+                //if there is an error use our fetchDataReject
+                if (err) dispatch(fetchDataRejected(err));
+                //We will set our loading state when fetching data is successful.
+                dispatch(fetchDataFulfilled(res.body.results));
+            })
     }
 }
 
-export default createStore(dataReducer, applyMiddleware(thunk))
+export default createStore(rootReducers, applyMiddleware(thunk, logger))
