b2eb612973629398b70feda58b4b9af3d291a0bf
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-04-04 22:24:26 +0200

    Create new action to update filtersSelected and filter properties

--- actions/filters.js
+++ actions/filters.js
@@ -1,4 +1,8 @@
-import { GET_FILTERS, SET_FILTERS } from '../constants'
+import { GET_FILTERS, SET_FILTERS, SET_FILTERS_SELECTED } from '../constants'
+import { setProperties } from './data'
+import store from '../store/index'
+import forEach from 'lodash/forEach'
+import filter from 'lodash/filter'
 
 export const getFilters = filters => {
     return {
@@ -11,5 +15,36 @@ export const setFilters = filters => {
     return {
         type: SET_FILTERS,
         payload: filters,
+    }
+}
+
+export const setFiltersSelected = filtersSelected => {
+    const state = store.getState()
+
+    let propertiesFiltered = state.dataReducer.data.customerProperties
+
+    forEach(filtersSelected, (value, key) => {
+        propertiesFiltered = filter(propertiesFiltered, property => {
+            if (value !== "") {
+                switch (key) {
+                    case "homeType":
+                        return property.propertyFeatures.featuresType === value
+                    case "operationType":
+                        return property.propertyOperation.operationType === value
+                    case "city":
+                        return property.propertyAddress.addressTown === value
+                    default:
+                        break;
+                }
+            }
+            return propertiesFiltered
+        })
+        // console.log("result1", propertiesFiltered)
+    })
+    // console.log("result2", propertiesFiltered)
+    store.dispatch(setProperties(propertiesFiltered))
+    return {
+        type: SET_FILTERS_SELECTED,
+        payload: filtersSelected
     }
 }
