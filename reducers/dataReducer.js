8140a27126eb769589bba69d391f80d2132dba5c
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-04-04 22:30:39 +0200

    Update reducer to use properties and add new action as SET_PROPERTIES

--- reducers/dataReducer.js
+++ reducers/dataReducer.js
@@ -1,8 +1,10 @@
-import { GET_DATA, GET_DATA_FULFILLED, GET_DATA_REJECTED } from '../constants'
+import { GET_DATA, GET_DATA_FULFILLED, GET_DATA_REJECTED, SET_PROPERTIES } from '../constants'
 
 const initialState = {
     //Have a data array responsible for getting the data and setting to the array.
     data: [],
+    dataFiltered: [],
+    properties: [],
     //Have the loading state indicate if it's done getting data.
     loading: true,
     //Have state for error message for recieving an error.
@@ -18,9 +20,11 @@ export default function (state = initialState, action) {
         case GET_DATA:
             return { ...state, loading: action.payload }
         case GET_DATA_FULFILLED:
-            return { ...state, data: action.payload, loading: action.loading }
+            return { ...state, data: action.payload, properties: action.payload.customerProperties, loading: action.loading }
         case GET_DATA_REJECTED:
             return { ...state, errorMessage: action.payload, loading: action.loading }
+        case SET_PROPERTIES: 
+            return { ...state, properties: action.payload}
         default:
             return state;
     }
