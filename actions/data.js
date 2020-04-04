d6ae3cc80d51c30a83ae9776a32bdffc6bf352ea
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-04-04 22:25:04 +0200

    New action created to update properties filtered

--- actions/data.js
+++ actions/data.js
@@ -1,4 +1,4 @@
-import { GET_DATA, GET_DATA_FULFILLED, GET_DATA_REJECTED } from '../constants'
+import { GET_DATA, GET_DATA_FULFILLED, GET_DATA_REJECTED, SET_PROPERTIES } from '../constants'
 //Define your action create that set your loading state.
 export const fetchData = bool => {
     //return a action type and a loading state indicating it is getting data. 
@@ -8,22 +8,31 @@ export const fetchData = bool => {
     }
 }
 
-//Define a action creator to set your loading state to false, and return the data when the promise is resolved
+//Define an action creator to set your loading state to false, and return the data when the promise is resolved
 export const fetchDataFulfilled = data => {
     //Return a action type and a loading to false, and the data.
     return {
         type: GET_DATA_FULFILLED,
         payload: data,
+        properties: data.customerProperties,
         loading: false,
     }
 }
 
-//Define a action creator that catches a error and sets an errorMessage
+//Define an action creator that catches a error and sets an errorMessage
 export const fetchDataRejected = error => {
     //Return a action type and a payload with a error
     return {
         type: GET_DATA_REJECTED,
         payload: error,
         loading: false,
+    }
+}
+
+//Define an action to set properties after filter 
+export const setProperties = properties => {
+    return {
+        type: SET_PROPERTIES,
+        payload: properties
     }
 }
