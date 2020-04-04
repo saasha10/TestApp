90767ce61d42abc54ddb901abe921d6d8a3d734d
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-04-04 22:26:53 +0200

    Add new constants

--- constants/index.js
+++ constants/index.js
@@ -1,3 +1,4 @@
+//DATA
 export const COUNTER_CHANGE = 'COUNTER_CHANGE'
 //Initiate the api call
 export const GET_DATA = 'GET_DATA'
@@ -5,6 +6,8 @@ export const GET_DATA = 'GET_DATA'
 export const GET_DATA_FULFILLED = 'GET_DATA_FULFILLED'
 //When there is a error return an errror action type. 
 export const GET_DATA_REJECTED = 'GET_DATA_REJECTED'
+//PROPERTIES
+export const SET_PROPERTIES = 'SET_PROPERTIES'
 //URL
 export const URL_HOME_BUSSINES_JSON = 'http://www.inmomallorcabusiness.com/panel/scripts/urlJSON.php'
 export const URL_HOME_BUSSINES = 'http://www.mallorcabusinesslife.com/panel/scripts/urlXCP.php'
@@ -12,3 +15,11 @@ export const URL_STAR_WARS = 'https://swapi.co/api/people'
 //FILTERS
 export const GET_FILTERS = 'GET_FILTERS'
 export const SET_FILTERS = 'SET_FILTERS'
+export const SET_FILTERS_SELECTED = 'SET_FILTERS_SELECTED'
+export const FILTERS_NAMES = {
+    HOME_TYPE: "homeType",
+    OPERATION_TYPE: "operationType",
+    CITY: "city",
+    FEATURES_BED_ROOM_NUMBER: "featuresBedRoomNumber",
+    OPERATION_PRICE: "operationPrice"
+}
