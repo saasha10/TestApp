4cf512504946934af838931e29cea8f313c83357
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-04-05 23:59:39 +0200

    Add two differents options to do any call to API

--- actions/getData.js
+++ actions/getData.js
@@ -1,8 +1,9 @@
 import Main from '../utilitiesJS/Main'
 import axios from 'axios'
+import superagent from 'superagent'
 import { fetchData, fetchDataFulfilled, fetchDataRejected } from './data'
 import { getFilters } from './filters'
-import { URL_HOME_BUSSINES_JSON } from '../constants'
+import { URL_HOME_BUSSINES, URL_HOME_BUSSINES_JSON } from '../constants'
 import minBy from 'lodash/minBy'
 import maxBy from 'lodash/maxBy'
 
@@ -37,5 +38,43 @@ export const getPeopleAxios = () => {
             dispatch(fetchDataFulfilled(res.data))
             //Error handle the promise and set your errorMessage
         }).catch(err => dispatch(fetchDataRejected(err)))
+    }
+}
+
+//Define your action creators that will be responsible for asynchronouse operatiosn 
+export const getPeopleAwait = () => {
+    //IN order to use await your callback must be asynchronous using async keyword.
+    return async dispatch => {
+        //Then perform your asynchronous operations.
+        try {
+            //Have it first fetch data from our starwars url.
+            const starWarsPromise = await fetch(URL_HOME_BUSSINES);
+            dispatch(fetchData(true));
+            //Then use the json method to get json data from api/
+            const people = await starWarsPromise.json();
+            console.log('people-----------', people);
+            //Now when the data is retrieved dispatch an action altering redux state.
+            dispatch(fetchDataFulfilled(people.results))
+        } catch (error) {
+            console.log('Getting People Error---------', error);
+            dispatch(fetchDataRejected(error))
+        }
+    }
+}
+
+
+export const getPeopleSuperAgent = () => {
+    return dispatch => {
+        //Dispatch the fetchData action creator before retrieving to set our loading state to true.
+        dispatch(fetchData(true));
+        //Then do a get request the get the err, and response callback, if there's an error dispatch the fetchDataRejected.
+        superagent.get(URL_HOME_BUSSINES)
+            //When the data is retrieved we will invoke the end method.
+            .end((err, res) => {
+                //if there is an error use our fetchDataReject
+                if (err) dispatch(fetchDataRejected(err));
+                //We will set our loading state when fetching data is successful.
+                dispatch(fetchDataFulfilled(res.body.results));
+            })
     }
 }
