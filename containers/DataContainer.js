78cfac6ea3a2fc7e321c222287e7ee14d004542d
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-04-09 19:30:31 +0200

    Using the new selector created

--- containers/DataContainer.js
+++ containers/DataContainer.js
@@ -1,12 +1,12 @@
 import { connect } from 'react-redux'
 import { getPeopleAxios } from '../actions/getData'
 import DataList from '../components/dataList'
-import { getProperties } from '../selectors/data'
+import { getProperties, getLoading } from '../selectors/data'
 
 //Map the redux state to your props.
 const mapStateToProps = state => ({
   properties: getProperties(state),
-  loading: state.dataReducer.loading
+  loading: getLoading(state)
 })
 
 //Map your action creators to your props.
