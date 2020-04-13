b7777a43280271f5fd6396441aad61bb1e3d0d60
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-04-13 20:37:33 +0200

    New component created to use DataList and filters modal

---
+++ components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+import ModalWithHooksContainer from '../containers/ModalWithHooksContainer'
+import DataContainer from '../containers/DataContainer'
+import { UIColors } from '../constants/colors'
+
+const StyledSafeAreaView = styled.SafeAreaView`
+  margin: 1px;
+`
+
+const StyledScrollView = styled.ScrollView`
+  background-color: ${UIColors.grayAlmostWhite} ; 
+  marginHorizontal: 1px;
+`
+
+export default ({ navigation }) => (
+    <>
+        <ModalWithHooksContainer />
+        <StyledSafeAreaView>
+            <StyledScrollView>
+                <DataContainer />
+            </StyledScrollView>
+        </StyledSafeAreaView>
+    </>
+)
