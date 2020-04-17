b525d504286734e51f30fa6c29ad00ab0d061b04
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-04-17 02:44:10 +0200

    Action and reducer navigation (future usability)?¿

---
+++ actions/navigation.js
@@ -0,0 +1,66 @@
+import { CommonActions, DrawerActions, TabActions } from '@react-navigation/native'
+import { NAVIGATE, GO_BACK_NAVIGATE, RESET_NAVIGATE, TAB_NAVIGATE, TOGGLE_DRAWER } from '../constants/types'
+import { navigationRef } from '../navigators/Navigator'
+
+export const navigateTo = route => ({
+    type: NAVIGATE,
+    route
+})
+
+export const goBackNavigate = () => ({
+    type: GO_BACK_NAVIGATE
+})
+
+export const resetNavigate = () => ({
+    type: RESET_NAVIGATE
+})
+
+export const tabNavigate = tab => ({
+    type: TAB_NAVIGATE,
+    tab
+})
+
+export const toggleDrawerNavigate = () => ({
+    type: TOGGLE_DRAWER
+})
+
+export const navigate = (routeName, params = {}) => dispatch => {
+    dispatch(navigateTo(routeName))
+    navigationRef.current?.dispatch(
+        CommonActions.navigate({
+            name: routeName,
+            params
+        })
+    )
+}
+
+export const goBack = () => dispatch => {
+    dispatch(goBackNavigate())
+    navigationRef.current?.dispatch(CommonActions.goBack())
+}
+
+export const reset = (routeName, params = {}) => dispatch => {
+    dispatch(resetNavigate())
+    navigationRef.dispatch(
+        CommonActions.reset({
+            name: routeName,
+            params
+        })
+    )
+}
+
+export const toggleDrawer = () => dispatch => {
+    dispatch(toggleDrawerNavigate())
+    navigationRef.current?.dispatch(
+        DrawerActions.toggleDrawer
+    )
+}
+
+export const navigateInTabs = tabName => dispatch => {
+    dispatch(tabNavigate(tabName))
+    navigationRef.current?.dispatch(
+        TabActions.jumpTo(
+            tabName
+        )
+    )
+}
---
+++ reducers/navigationReducer.js
@@ -0,0 +1,37 @@
+import { NAVIGATE, GO_BACK_NAVIGATE, RESET_NAVIGATE, TAB_NAVIGATE, TOGGLE_DRAWER } from '../constants/types'
+
+const initialState = {
+    isDrawerOpen: false,
+    stackView: "Main",
+    tabView: "Home"
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case NAVIGATE:
+            return {
+                ...state,
+                stackView: state.stackView.concat([action.route])
+            }
+        case GO_BACK_NAVIGATE:
+            return {
+                ...state,
+                stackView: state.stackView.pop
+            }
+        case RESET_NAVIGATE:
+            return initialState
+
+        case TAB_NAVIGATE:
+            return {
+                ...state,
+                tabView: state.tabView.concat([action.tab])
+            }
+        case TOGGLE_DRAWER:
+            return {
+                ...state,
+                isDrawerOpen: !state.isDrawerOpen
+            }
+        default:
+            return state
+    }
+}
