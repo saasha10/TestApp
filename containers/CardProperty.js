1388f1704f547b07514ba0e06cb6e52c90cb50a2
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-04-17 21:26:34 +0200

    Using the new ACTION NAVIGATE

--- containers/CardProperty.js
+++ containers/CardProperty.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux'
 import { addFavouriteProperty, removeFavouriteProperty } from '../actions/properties'
+import { navigate } from '../actions/navigation'
 import CardProperty from '../components/utilities/CardProperty'
 import { getFavouriteProperties } from '../selectors/favouriteProperties'
 
@@ -9,7 +10,8 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
     addFavouriteProperty: state => dispatch(addFavouriteProperty(state)),
-    removeFavouriteProperty: state => dispatch(removeFavouriteProperty(state))
+    removeFavouriteProperty: state => dispatch(removeFavouriteProperty(state)),
+    navigate: state => dispatch(navigate(state))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(CardProperty)
