56e57f88cabb1487b1e725ea222233b14c875083
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-04-17 03:57:24 +0200

    Favourite Properties component created

--- components/FavouritesProperties.js
+++ components/FavouritesProperties.js
@@ -1,16 +1,49 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { ScrollView } from 'react-native'
 import { Text } from 'react-native-elements'
+import { StyledViewContainer } from './dataList'
+import CardProperty from '../containers/CardProperty'
+import { UIColors } from '../constants/colors'
+import styled from 'styled-components'
 
-export default class FavouriteProperties extends Component {
+const WrapperTitle = styled.View`
+    align-items: center;
+    justify-content: center;
+    height: 50px;
+    background-color: ${UIColors.blue};
 
+`
 
+const Title = styled.Text`
+    margin: 5px;
+    font-size: 24px;
+    color: ${UIColors.white};
+`
+
+const NoPropertyText = styled(Text)`
+    align-items: center;
+    justify-content: center;
+    font-size: 24px;
+`
+
+class FavouriteProperties extends Component {
     render() {
+        const { favouriteProperties } = this.props
 
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text h2 >Favourite Properties</Text>
-            </View>
+            <>
+                <WrapperTitle>
+                    <Title>My properties</Title>
+                </WrapperTitle>
+                <ScrollView>
+                    <StyledViewContainer>
+                        {favouriteProperties.length ? favouriteProperties.map((property, key) => <CardProperty key={key} property={property} />)
+                            : <NoPropertyText h6>You don't have favourite properties added</NoPropertyText>}
+                    </StyledViewContainer>
+                </ScrollView>
+            </>
         )
     }
 }
+
+export default FavouriteProperties
