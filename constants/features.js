cbcaba278947209f74f45ca281bd9b84b36851a1
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-04-25 17:22:48 +0200

    New features constants created

---
+++ constants/features.js
@@ -0,0 +1,29 @@
+export const FEATURES = {
+    BUILT_YEAR: "featuresBuiltYear",
+    AREA_CONSTRUCTED: "featuresAreaConstructed",
+    AREA_PLOT: "featuresAreaPlot",
+    AREA_USABLE: "featuresAreaUsable",
+    BATHROOM: "featuresBathroomNumber",
+    BEDROOM: "featuresBedroomNumber",
+    ENERGY_CERTIFICATE: "featuresEnergyCertificaing",
+    DOORMAN: "featuresDoorman",
+    HEATING: "featuresHeating",
+    HEATING_TYPE: "featuresHeatingType",
+    CONDITIONED_AIR: "featuresConditionedAir",
+    TYPE_CONDITIONED_AIR: "featuresConditionedAirType",
+    ORIENTATION: {
+        NORTH: "featuresOrientationNorth",
+        WEST: "featuresOrientationWest",
+        EAST: "featuresOrientationEast",
+        SOUTH: "featuresOrientationSouth"
+    },
+    PARKING: "featuresParkingAvailable",
+    POOL: "featuresPool",
+    TERRACE: "featuresTerrace",
+    GARDEN: "featuresGarden",
+    WARDROBES: "featuresWardrobes",
+    STORAGE: "featuresStorage",
+    EQUIPPED: "featuresEquippedWithFurniture",
+    LIFT: "featuresLiftAvailable",
+    PENT_HOUSE: "featuresPenthouse"
+}
