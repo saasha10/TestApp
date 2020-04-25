9f172cf9bb6beb775dae549e2f70bcd97eb33342
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-04-25 17:23:16 +0200

    Two new functions created

--- utilitiesJS/Main.js
+++ utilitiesJS/Main.js
@@ -1,5 +1,7 @@
+import { FEATURES } from '../constants/features'
+const { BATHROOM, BEDROOM, HEATING, CONDITIONED_AIR, ORIENTATION, PARKING, POOL, TERRACE, GARDEN, WARDROBES, STORAGE, EQUIPPED, LIFT, PENT_HOUSE, DOORMAN } = FEATURES
 export default class Main {
-    constructor(){
+    constructor() {
     }
     //--------------------------------------------------------------------------
     /**
@@ -8,16 +10,99 @@ export default class Main {
      * @param wordToSearch2 -> parameter to find something
      */
     newArrayDifferentValues = (arrayList, wordToSearch1, wordToSearch2) => {
-        // console.log(arrayList)
-        // console.log(wordToSearch1)
-        // console.log(wordToSearch2)
         let result = []
 
-        arrayList.forEach(element => {            
+        arrayList.forEach(element => {
             if (result.indexOf(element[wordToSearch1][wordToSearch2]) == -1)
                 result.push(element[wordToSearch1][wordToSearch2])
-        });
-        //console.log(result)
+        })
+
         return result
+    }
+    //--------------------------------------------------------------------------
+    parserFeatureName = key => {
+        switch (key) {
+            case BATHROOM:
+                return 'Bathroom'
+            case BEDROOM:
+                return 'Bedroom'
+            case DOORMAN:
+                return 'Doorman'
+            case HEATING:
+                return 'Heating'
+            case CONDITIONED_AIR:
+                return 'Conditioned air'
+            case ORIENTATION.NORTH:
+                return 'North orientation'
+            case ORIENTATION.WEST:
+                return 'West orientation'
+            case ORIENTATION.EAST:
+                return 'East orientation'
+            case ORIENTATION.SOUTH:
+                return 'South orientation'
+            case PARKING:
+                return 'Parking'
+            case POOL:
+                return 'Pool'
+            case TERRACE:
+                return 'Terrace'
+            case GARDEN:
+                return 'Garden'
+            case WARDROBES:
+                return 'Wardrobe'
+            case STORAGE:
+                return 'Warehouse'
+            case EQUIPPED:
+                return 'Equipped'
+            case LIFT:
+                return 'Lift'
+            case PENT_HOUSE:
+                return 'Pent house'
+            default:
+                //console.log("NOT FOUND feature", key)
+                return key
+        }
+    }
+    //--------------------------------------------------------------------------
+    parserNameToIcon = key => {
+        switch (key) {
+            case BATHROOM:
+                return 'bath'
+            case BEDROOM:
+                return 'bed'
+            case DOORMAN:
+                return 'person-booth'
+            case HEATING:
+                return 'gripfire'
+            case CONDITIONED_AIR:
+                return 'snowflake'
+            case ORIENTATION.NORTH:
+            case ORIENTATION.WEST:
+                return 'circle'
+            case ORIENTATION.EAST:
+            case ORIENTATION.SOUTH:
+                return 'circle'
+            case PARKING:
+                return 'car'
+            case POOL:
+                return 'swimmer'
+            case TERRACE:
+                return 'circle'
+            case GARDEN:
+                return 'tree'
+            case WARDROBES:
+                return 'circle'
+            case STORAGE:
+                return 'warehouse'
+            case EQUIPPED:
+                return 'circle'
+            case LIFT:
+                return 'circle'
+            case PENT_HOUSE:
+                return 'circle'
+            default:
+                //console.log("NOT FOUND ICON", key)
+                return 'angry'
+        }
     }
 }
