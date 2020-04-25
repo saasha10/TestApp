9cd7ea06fd960867b0f587b5108ba359fb3cb742
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-04-25 17:25:04 +0200

    Add FlatList element with property details

--- components/utilities/MainProperty.js
+++ components/utilities/MainProperty.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import { View } from 'react-native'
-import { Card, Text, Icon } from 'react-native-elements'
-import IconFontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+import { FlatList, SafeAreaView } from 'react-native'
+import { Card, Text, Badge } from 'react-native-elements'
+import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5'
 import styled from 'styled-components'
 import { UIColors } from '../../constants/colors'
+import Main from '../../utilitiesJS/Main'
 import get from 'lodash/get'
-import map from 'lodash/map'
+import { screenWidth } from '../ModalWithHooks'
 
-
+const main = new Main()
 
 const StyledScrollView = styled.ScrollView`
   background-color: ${UIColors.grayAlmostWhite};
@@ -28,42 +29,48 @@ const TextPriceAndTown = styled.Text`
     text-transform: capitalize;
 `
 
-createFeature = features => map(features, (el, key) => {
-    console.log({ key, el })
-    const showFeature = key !== 'featuresAreaConstructed' && key !== 'featuresAreaPlot' && key !== 'featuresAreaUsable' && key !== 'featuresEnergyCertificateRating' && key !== 'featuresConservation'
-    if (showFeature) {
-        if (typeof el !== "boolean") {
-            return (
-                <React.Fragment key={key}>
-                    <Icon name='bath'
-                        type='font-awesome'
-                        size={20}
-                        color='blue'
-                    />
-                    <Text>{key}</Text>
-                    <Text>{el}</Text>
-                </React.Fragment>
-            )
-        }
-        return (
-            <React.Fragment key={key}>
-                <Icon name='bath'
-                    type='font-awesome'
-                    size={20}
-                    color='blue'
-                />
-                <Text>{key}</Text>
-            </React.Fragment>
-        )
-    }
-})
+const WrapperFeature = styled.View`
+    flex-direction: row;
+    justify-content: space-around;
+    margin: 1px;
+`
+
+const FeatureName = styled.Text`
+    font-size: 18px;
+    margin: 2%;
+`
+
+function Feature({ feature }) {
+    const key = Object.keys(feature)
+    const value = Object.values(feature)
+    const showValue = typeof value[0] !== "boolean"
+
+    return (
+        <WrapperFeature>
+            <FontAwesome5Icon name={main.parserNameToIcon(key[0])}
+                type='font-awesome'
+                size={20}
+            />
+            <FeatureName>{main.parserFeatureName(key[0])}</FeatureName>
+            {showValue && <Badge value={value[0]} textStyle={{fontSize: 20, padding: 5}}/>}
+        </WrapperFeature>
+    )
+}
 
 export default function MainProperty({ property }) {
     const address = get(property, 'propertyAddress')
     const contact = get(property, '`propertyContact')
     const descriptions = get(property, 'propertyDescriptions')
-    const features = get(property, 'propertyFeatures')
-    const featuresType = get(features, 'featuresType')
+    const featuresObj = get(property, 'propertyFeatures')
+    let featuresArray = []
+    Object.entries(featuresObj).forEach(([key, value]) => {
+        const showFeature = key !== 'featuresAreaConstructed' && key !== 'featuresAreaPlot' && key !== 'featuresAreaUsable' &&
+            key !== 'featuresEnergyCertificateRating' && key !== 'featuresConservation' && key !== 'featuresTypeflat' && key !== 'featuresType' 
+            && key !== 'featuresBuiltYear' && key !== 'featuresConditionedAirType' && key !== 'featuresHeatingType'
+        if (showFeature) featuresArray.push({ [key]: value })
+    })
+
+    const featuresType = get(featuresObj, 'featuresType')
     const images = get(property, 'propertyImages')
     const operation = get(property, 'propertyOperation')
     const price = get(operation, 'operationPrice')
@@ -83,53 +90,15 @@ export default function MainProperty({ property }) {
                     <TextPriceAndTown>{city}</TextPriceAndTown>
                 </PriceAndTown>
             </Card>
-            <Card style={{ marginTop: 1, marginHorizontal: 0 }}>
-                <View style={{ justifyContent: "space-between" }}>
-                    <StyledScrollView>
-                        {this.createFeature(features)}
-                    </StyledScrollView>
-                </View>
-                {/* <View>
-                    <Icon
-                        name='bath'
-                        type='font-awesome'
-                        size={20}
-                        color='blue'
-                    />
-                    <Icon
-                        name='bed'
-                        type='font-awesome'
-                        size={20}
-                        color='red'
-                    />
-                    <Icon
-                        name='check'
-                        type='font-awesome'
-                        size={20}
-                        color='grey'
-                    />
-                    <Icon
-                        name='tree'
-                        type='font-awesome'
-                        size={20}
-                        color='green'
-                    />
-                    <IconFontAwesome5
-                        name='parking'
-                        size={20}
-                        color='black'
-                    />
-                    <IconFontAwesome5
-                        name='swimmer'
-                        size={20}
-                        color='blue'
-                    />
-                    <IconFontAwesome5
-                        name='warehouse'
-                        size={20}
-                        color='black'
-                    />
-                </View> */}
+            <Card style={{ marginTop: 1, width: screenWidth}}>
+                <SafeAreaView style={{flex: 1}}>
+                    <FlatList
+                        data={featuresArray}
+                        renderItem={({ item }) => <Feature feature={item} />}
+                        numColumns={2}
+                        columnWrapperStyle={{flex: 1, justifyContent: "space-around", alignItems: "center"}}
+                        keyExtractor={(value, index) => index.toString()} />
+                </SafeAreaView>
             </Card>
         </StyledScrollView>
     )
