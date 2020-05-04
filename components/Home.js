0bfcf5be02d99639499535852bdb099b14863bcb
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-05-04 19:44:48 +0200

    Testing the new dependency installed

--- components/Home.js
+++ components/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
+import { StyleSheet, View } from 'react-native'
 import ModalWithHooksContainer from '../containers/ModalWithHooksContainer'
 import DataContainer from '../containers/DataContainer'
 import { UIColors } from '../constants/colors'
+import MapView, { PROVIDER_GOOGLE, Circle } from 'react-native-maps'
 
 const StyledSafeAreaView = styled.SafeAreaView`
   margin: 1px;
@@ -13,13 +15,47 @@ const StyledScrollView = styled.ScrollView`
   marginHorizontal: 1px;
 `
 
+const LatLng = {
+  latitude: 41.390290,
+  longitude: 2.155351,
+  latitudeDelta: 0.015,
+  longitudeDelta: 0.0121,
+}
+
 export default () => (
-    <>
-        <ModalWithHooksContainer />
-        <StyledSafeAreaView>
-            <StyledScrollView>
-                <DataContainer />
-            </StyledScrollView>
-        </StyledSafeAreaView>
-    </>
+  <>
+    {/* <ModalWithHooksContainer /> */}
+    {/* <View style={styles.container}> */}
+    <MapView
+      provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+      style={styles.map}
+      region={LatLng}
+    >
+      <Circle
+        center={LatLng}
+        radius={200} 
+        strokeColor='#f9fafb'
+        fillColor='rgba(183, 183, 183, 0.5)'/>
+    </MapView>
+
+    {/* </View> */}
+    {/* <StyledSafeAreaView>
+      <StyledScrollView>
+        <DataContainer />
+      </StyledScrollView>
+    </StyledSafeAreaView> */}
+  </>
 )
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
