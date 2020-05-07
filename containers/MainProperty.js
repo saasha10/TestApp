f1fd485005aca0b7e3bc0869bc08e3880c31aafb
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-05-07 21:10:28 +0200

    Using the new selector and add new action to change Screen

--- containers/MainProperty.js
+++ containers/MainProperty.js
@@ -1,8 +1,14 @@
 import { connect } from 'react-redux'
+import { navigate } from '../actions/navigation'
 import MainProperty from '../components/utilities/MainProperty'
+import { getPropertySelected } from '../selectors/propertySelected'
 
 const mapStateToProps = state => ({
-    property: state.propertySelected
+    property: getPropertySelected(state)
 })
 
-export default connect(mapStateToProps)(MainProperty)
+const mapDispatchToProps = dispatch => ({
+    navigate: state => dispatch(navigate(state))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainProperty)
