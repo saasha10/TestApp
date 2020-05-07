c677056ff3d3159c29bee1e8bad52ad80d7b7f3a
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-05-07 21:12:29 +0200

    Create the new two buttons to 'call' and 'Book visit'

--- components/utilities/MainProperty.js
+++ components/utilities/MainProperty.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import { FlatList, View } from 'react-native'
-import { Card, Text, Badge, Divider } from 'react-native-elements'
+import { Platform, Linking, FlatList, View } from 'react-native'
+import { Card, Text, Badge, Divider, Button } from 'react-native-elements'
 import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5'
 import styled from 'styled-components'
+import { STACK_NAMES } from '../../constants/navigation'
 import { UIColors } from '../../constants/colors'
 import Main from '../../utilitiesJS/Main'
 import { screenWidth, screenHeight } from '../ModalWithHooks'
 import PanelCollapsible from './PanelCollapsible'
 import get from 'lodash/get'
 import MapView, { PROVIDER_GOOGLE, Circle } from 'react-native-maps'
+
+const { BOOK_VISIT } = STACK_NAMES
 
 const heightMap = Math.floor(screenHeight / 2)
 
@@ -31,7 +34,7 @@ const PriceAndTown = styled.View`
 `
 
 const TextPriceAndTown = styled.Text`
-    font-size: 15px;
+    font-size: 17px;
     text-transform: capitalize;
 `
 
@@ -59,6 +62,15 @@ const StyledMapView = styled(MapView)`
     height: ${heightMap};
     width: ${screenWidth};
 `
+
+makeCall = phone => {
+    let phoneNumber = `telprompt:${phone}`
+
+    if (Platform.OS === 'android') {
+        phoneNumber = `tel:${phone}`
+    }
+    Linking.openURL(phoneNumber)
+}
 
 createFeaturesArray = features => {
     let result = []
@@ -104,7 +116,7 @@ function Feature({ feature }) {
     )
 }
 
-export default function MainProperty({ property }) {
+export default function MainProperty({ property, navigate }) {
     const address = get(property, 'propertyAddress')
     const latLng = {
         latitude: get(address, 'addressCoordinatesLatitude'),
@@ -112,7 +124,8 @@ export default function MainProperty({ property }) {
         latitudeDelta: 0.015,
         longitudeDelta: 0.0121,
     }
-    const contact = get(property, '`propertyContact')
+    const contact = get(property, 'propertyContact')
+    const phoneNumber = get(contact, 'contactPrimaryPhoneNumber')
     const descriptions = get(property, 'propertyDescriptions')
     const featuresObj = get(property, 'propertyFeatures')
     const featuresArray = createFeaturesArray(featuresObj)
@@ -124,40 +137,69 @@ export default function MainProperty({ property }) {
 
 
     return (
-        <StyledScrollView>
-            <Card
-                image={{ uri: images[0].imageUrl }}
-                imageStyle={{ height: 300 }}
-                containerStyle={{ margin: 0 }}
-            >
-                <TypeHouse h3>{featuresType}</TypeHouse>
-                <PriceAndTown>
-                    <TextPriceAndTown>{(price).toLocaleString('de-DE')} €</TextPriceAndTown>
-                    <TextPriceAndTown>{city}</TextPriceAndTown>
-                </PriceAndTown>
-            </Card>
-            <Divider style={{ backgroundColor: UIColors.lightGray, paddingBottom: 4 }} />
-            <FlatList
-                data={formatData(featuresArray, numColumns)}
-                style={{ flex: 1 }}
-                renderItem={({ item }) => <Feature feature={item} />}
-                numColumns={numColumns}
-                keyExtractor={(value, index) => index.toString()} />
-            <PanelCollapsible title="Description">
-                <Text>{descriptions[0].descriptionText}</Text>
-            </PanelCollapsible>
-            <StyledMapWrapper>
-                <StyledMapView
-                    provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-                    region={latLng}
+        <>
+            <StyledScrollView>
+                <Card
+                    image={{ uri: images[0].imageUrl }}
+                    imageStyle={{ height: 300 }}
+                    containerStyle={{ margin: 0 }}
                 >
-                    <Circle
-                        center={latLng}
-                        radius={200}
-                        strokeColor='#f9fafb'
-                        fillColor='rgba(183, 183, 183, 0.5)' />
-                </StyledMapView>
-            </StyledMapWrapper>
-        </StyledScrollView>
+                    <TypeHouse h3>{featuresType}</TypeHouse>
+                    <PriceAndTown>
+                        <TextPriceAndTown>{(price).toLocaleString('de-DE')} €</TextPriceAndTown>
+                        <TextPriceAndTown>{city}</TextPriceAndTown>
+                    </PriceAndTown>
+                </Card>
+                <Divider style={{ backgroundColor: UIColors.lightGray, paddingBottom: 4 }} />
+                <FlatList
+                    data={formatData(featuresArray, numColumns)}
+                    style={{ flex: 1 }}
+                    renderItem={({ item }) => <Feature feature={item} />}
+                    numColumns={numColumns}
+                    keyExtractor={(value, index) => index.toString()} />
+                <PanelCollapsible title="Description">
+                    <Text>{descriptions[0].descriptionText}</Text>
+                </PanelCollapsible>
+                <StyledMapWrapper>
+                    <StyledMapView
+                        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                        region={latLng}
+                    >
+                        <Circle
+                            center={latLng}
+                            radius={200}
+                            strokeColor='#f9fafb'
+                            fillColor='rgba(183, 183, 183, 0.5)' />
+                    </StyledMapView>
+                </StyledMapWrapper>
+            </StyledScrollView>
+            <Button
+                icon={
+                    <FontAwesome5Icon
+                        name="phone"
+                        size={25}
+                        color="white"
+                    />
+                }
+                onPress={() => makeCall(phoneNumber)}
+                buttonStyle={{ borderRadius: 50, backgroundColor: 'green' }}
+                containerStyle={{ position: "absolute", bottom: 10, right: 100 }}
+            />
+            <Button
+                icon={
+                    <FontAwesome5Icon
+                        name="calendar-check"
+                        size={25}
+                        color="white"
+                    />
+                }
+                iconRight
+                title="Visit"
+                onPress={() => navigate(BOOK_VISIT)}
+                buttonStyle={{ borderRadius: 25, backgroundColor: 'red' }}
+                titleStyle={{ fontSize: 18, paddingRight: 4 }}
+                containerStyle={{ position: "absolute", bottom: 10, right: 10 }}
+            />
+        </>
     )
 }
