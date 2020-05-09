dac4a4b96ba92c86e5abf48efa1fdf1fedf76f7c
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-05-09 13:48:51 +0200

    Create a new selector to get propertyReference

--- selectors/propertySelected.js
+++ selectors/propertySelected.js
@@ -1,3 +1,10 @@
 import get from 'lodash/fp/get'
+import pipe from 'lodash/fp/pipe'
+import { PROPERTY_REFERENCE } from '../constants'
 
 export const getPropertySelected = get('propertySelected')
+
+export const getpropertyReference = pipe(
+    getPropertySelected,
+    get(PROPERTY_REFERENCE)
+)
