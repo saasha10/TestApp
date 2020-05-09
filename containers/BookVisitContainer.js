8a0be510ed053f078ca36d7528944bdb08ead328
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-05-09 13:49:40 +0200

    Change property to just reference

--- containers/BookVisitContainer.js
+++ containers/BookVisitContainer.js
@@ -1,10 +1,10 @@
 import { connect } from 'react-redux'
-import { navigate } from '../actions/navigation'
+// import { navigate } from '../actions/navigation'
 import BookVisit from '../components/BookVisit'
-import { getPropertySelected } from '../selectors/propertySelected'
+import { getpropertyReference } from '../selectors/propertySelected'
 
 const mapStateToProps = state => ({
-    property: getPropertySelected(state)
+    propertyReference: getpropertyReference(state)
 })
 
 export default connect(mapStateToProps)(BookVisit)
