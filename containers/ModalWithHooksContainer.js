a72402d3932b9fa563fb2362dff7c7b719b8e8fa
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-05-12 18:05:23 +0200

    Add new HOCC create to do translate

--- containers/ModalWithHooksContainer.js
+++ containers/ModalWithHooksContainer.js
@@ -1,4 +1,6 @@
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
+import { compose } from 'redux'
+import { connectLocale } from '../components/HOCC/connectLocale'
 import ModalWithHooks from '../components/ModalWithHooks'
 import { setFiltersSelected } from '../actions/filters'
 import { getFilters } from '../selectors/filters'
@@ -11,4 +13,8 @@ const mapDispatchToProps = dispatch => ({
     setFiltersSelected: state => dispatch(setFiltersSelected(state))
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(ModalWithHooks)
+export default compose(
+    connectLocale,
+    connect(
+        mapStateToProps, mapDispatchToProps))
+    (ModalWithHooks)
