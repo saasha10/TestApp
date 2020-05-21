74a3b840c3eec18a127f4d9ed387f898d010d48f
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-05-21 16:41:00 +0200

    Add TRANSLATION to FavouriteProperties

--- containers/FavouriteProperties.js
+++ containers/FavouriteProperties.js
@@ -1,10 +1,14 @@
 import { connect } from 'react-redux'
-import { addFavouriteProperty, removeFavouriteProperty } from '../actions/properties'
+import { compose } from 'redux'
+import { connectLocale } from '../components/HOCC/connectLocale'
 import FavouritesProperties from '../components/FavouritesProperties'
 import { getFavouriteProperties } from '../selectors/favouriteProperties'
 
 const mapStateToProps = state => ({
     favouriteProperties: getFavouriteProperties(state)
-})
+}) 
 
-export default connect(mapStateToProps)(FavouritesProperties)
+export default compose(
+    connectLocale,
+    connect(mapStateToProps))
+    (FavouritesProperties)
