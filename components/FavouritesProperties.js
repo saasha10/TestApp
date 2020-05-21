dc71313aba1f49dc809eeeeea1ee8de72b26a506
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-05-21 16:41:56 +0200

    Using translation, improve style and add Logo

--- components/FavouritesProperties.js
+++ components/FavouritesProperties.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { ScrollView } from 'react-native'
+import { ScrollView, Image } from 'react-native'
 import { Text } from 'react-native-elements'
 import { StyledViewContainer } from './dataList'
 import CardProperty from '../containers/CardProperty'
@@ -19,27 +19,45 @@ export const Title = styled.Text`
     color: ${UIColors.white};
 `
 
+const WrapperNoProperty = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    background-color: ${UIColors.white};
+`
+
 const NoPropertyText = styled(Text)`
     align-items: center;
     justify-content: center;
-    font-size: 24px;
+    font-size: 18px;
+    font-family: serif;
 `
 
 class FavouriteProperties extends Component {
     render() {
-        const { favouriteProperties } = this.props
+        const { favouriteProperties, message } = this.props
 
         return (
             <>
                 <WrapperTitle>
-                    <Title>My properties</Title>
+                    <Title>{message['My properties']}</Title>
                 </WrapperTitle>
-                <ScrollView>
-                    <StyledViewContainer>
-                        {favouriteProperties.length ? favouriteProperties.map((property, key) => <CardProperty key={key} property={property} />)
-                            : <NoPropertyText h6>You don't have favourite properties added</NoPropertyText>}
-                    </StyledViewContainer>
-                </ScrollView>
+                {favouriteProperties.length ?
+                    <ScrollView>
+                        <StyledViewContainer>
+                            {favouriteProperties.map((property, key) => <CardProperty key={key} property={property} />)}
+                        </StyledViewContainer>
+                    </ScrollView>
+                    :
+                    <WrapperNoProperty>
+                        <Image source={require('../img/LogoHomeBusinessLife.png')}
+                            style={{ width: 250, height: 250 }}
+                            resizeMethod='resize'
+                            resizeMode='center'
+                        />
+                        <NoPropertyText>{message['Any property added']}</NoPropertyText>
+                    </WrapperNoProperty>
+                }
             </>
         )
     }
