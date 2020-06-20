f2825c95763e869a658034dd74285fc810dc3c3f
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-06-20 12:31:00 +0200

    Create SignIn component

--- login/SignIn.js
+++ login/SignIn.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react'
-import Icon from 'react-native-vector-icons/FontAwesome'
-import { Text, Input } from 'react-native-elements'
+import React, { useState } from 'react'
+import { Image } from 'react-native'
+import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5'
+import { Input, Button } from 'react-native-elements'
 import styled from 'styled-components/native'
 import { UIColors } from '../constants/colors'
+import { connectLocale } from '../components/HOCC/connectLocale'
+import { StyledInput, StyledIcon, sizeIcon } from '../components/BookVisit'
 
 const StyledViewSignIn = styled.View`
     flex: 1;
@@ -11,20 +14,63 @@ const StyledViewSignIn = styled.View`
     background-color: ${UIColors.grayAlmostWhite};
 `
 
-export default function SignIn() {
+validateEmail = email => {
+    const validate = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return validate.test(email.toLowerCase())
+}
+
+function SignIn({ message }) {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [checkEmail, setCheckEmail] = useState(true)
+
+
     return (
         <StyledViewSignIn>
-            <Text h1>Sign in</Text>
-            <Text>This is just a test</Text>
+            <Image source={require('../img/LogoHomeBusinessLife.png')}
+                style={{ width: 250, height: 250 }}
+            />
             <Input
-                placeholder="Type your email"
+                placeholder={message['Type your email']}
                 leftIcon={
-                    <Icon
-                        name='user'
+                    <FontAwesome5Icon
+                        name='user-alt'
                         size={24}
-                        color='black' />
-                }
+                    />}
+                containerStyle={StyledInput}
+                leftIconContainerStyle={StyledIcon}
+                value={email}
+                onChangeText={setEmail}
+                errorMessage={checkEmail ? '' : 'Enter a correct email'}
+                renderErrorMessage
+                onBlur={() => {
+                    const validEmail = validateEmail(email)
+                    setCheckEmail(validEmail)
+                }}
+            />
+            <Input
+                placeholder={message['Your password']}
+                secureTextEntry={true}
+                leftIcon={
+                    <FontAwesome5Icon
+                        name='lock'
+                        size={sizeIcon}
+                    />}
+                containerStyle={StyledInput}
+                leftIconContainerStyle={StyledIcon}
+                value={password}
+                onChangeText={setPassword}
+            />
+            <Button
+                title="Sign in"
+                titleStyle={{ fontSize: 26 }}
+                type="outline"
+                raised
+                containerStyle={{ marginTop: 30 }}
             />
         </StyledViewSignIn>
     )
 }
+
+export default connectLocale(SignIn)
