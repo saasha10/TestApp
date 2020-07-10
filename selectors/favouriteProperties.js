7a8730670cb9a35efc02e7d7442213e43c746c10
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-07-10 13:42:01 +0200

    Improve selectors to check favouriteProperties Help: Ruy Mallo

--- selectors/favouriteProperties.js
+++ selectors/favouriteProperties.js
@@ -1,5 +1,19 @@
+import pipe from 'lodash/fp/pipe'
 import get from 'lodash/fp/get'
+import includes from 'lodash/fp/includes'
+import map from 'lodash/fp/map'
 import { FAVOURITE_PROPERTIES } from '../constants/favouriteProperties'
 
 
 export const getFavouriteProperties = get(FAVOURITE_PROPERTIES)
+
+export const getFavouritePropertiesReferences = pipe(
+    getFavouriteProperties,
+    map('propertyReference')
+)
+
+export const isFavouriteProperty = (state, property) => 
+    pipe(
+        getFavouritePropertiesReferences,
+        includes(property.propertyReference)
+    )(state)
